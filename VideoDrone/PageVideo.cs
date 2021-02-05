3f228a08795f55d0ea2a01f461c9d7cb042881ba
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2021-02-05 14:38:09 +0100

    Add files via upload

---
+++ VideoDrone/PageVideo.cs
@@ -0,0 +1,118 @@
+﻿using DJI.WindowsSDK;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using DJIVideoParser;
+using System.Runtime.CompilerServices;
+
+namespace DJIDrone.FPV
+{
+    public sealed partial class PageVideoDrone : Page
+    {
+        private Parser videoParser;
+
+        public PageVideoDrone()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            InitializeVideoFeedModule();
+            await DJISDKManager.Instance.ComponentManager.GetCameraHandler(0, 0).SetCameraWorkModeAsync(new CameraWorkModeMsg { value = CameraWorkMode.SHOOT_PHOTO });
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            UninitializeVideoFeedModule(); // A TESTER
+        }
+
+
+        /// <summary>
+        /// Cette fonction initialise les flux vidéo.
+        /// </summary>
+        private async void InitializeVideoFeedModule()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+            {
+                if (videoParser == null)
+                {
+                    videoParser = new Parser();
+                    videoParser.Initialize(delegate (byte[] data)
+                    {
+                        return DJISDKManager.Instance.VideoFeeder.ParseAssitantDecodingInfo(0, data);
+                    });
+                    //Affiche et met les données
+                    videoParser.SetSurfaceAndVideoCallback(0, 0, swapChainPanel, ReceiveDecodedData);
+                    DJISDKManager.Instance.VideoFeeder.GetPrimaryVideoFeed(0).VideoDataUpdated += OnVideoPush;
+                }
+                //Cherche le type de caméra
+                DJISDKManager.Instance.ComponentManager.GetCameraHandler(0, 0).CameraTypeChanged += OnCameraTypeChanged;
+                var type = await DJISDKManager.Instance.ComponentManager.GetCameraHandler(0, 0).GetCameraTypeAsync();
+                OnCameraTypeChanged(this, type.value);
+            });
+        }
+
+
+        private void UninitializeVideoFeedModule()
+        {
+            if (DJISDKManager.Instance.SDKRegistrationResultCode == SDKError.NO_ERROR)
+            {
+                videoParser.SetSurfaceAndVideoCallback(0, 0, null, null);
+                DJISDKManager.Instance.VideoFeeder.GetPrimaryVideoFeed(0).VideoDataUpdated -= OnVideoPush;
+            }
+        }
+        void OnVideoPush(VideoFeed sender, byte[] bytes)
+        {
+            videoParser.PushVideoData(0, 0, bytes, bytes.Length);
+        }
+
+        /// <summary>
+        /// Retourne l'image en RGBA
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        async void ReceiveDecodedData(byte[] data, int width, int height)
+        {
+        }
+
+        /// <summary>
+        /// Met le type de la caméra
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="value"></param>
+        
+        private void OnCameraTypeChanged(object sender, CameraTypeMsg? value)
+        {
+            if (value != null)
+            {
+                switch (value.Value.value)
+                {
+                    case CameraType.MAVIC_AIR_CAMERA:
+                        this.videoParser.SetCameraSensor(AircraftCameraType.Others);
+                        break;
+                    default:
+                        this.videoParser.SetCameraSensor(AircraftCameraType.Others);
+                        break;
+                }
+
+            }
+        }
+
+    }
+}
