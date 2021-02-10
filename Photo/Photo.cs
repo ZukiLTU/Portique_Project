1cf06545caa5f9afbf391f2fc1a43b2e85a6714f
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2021-02-10 10:43:39 +0100

    Add files via upload

---
+++ Photo/Photo.cs
@@ -0,0 +1,73 @@
+﻿using DJI.WindowsSDK;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using DJIVideoParser;
+using DJI.WindowsSDK.Components;
+
+namespace DJIDrone.VideoDrone
+{
+    public sealed partial class PageVideo : Page
+    {
+        /// <summary>
+        /// Met le mode de la caméra
+        /// </summary>
+        public void SwitchMode()
+        {
+            DJISDKManager.Instance.ComponentManager.GetCameraHandler(0, 0).CameraWorkModeChanged += async delegate (object sender, CameraWorkModeMsg? val)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async () =>
+                {
+                    if(val != null)
+                    {
+                        lblMode.Text = "Mode : " + val.Value.value.ToString();
+                    }
+                });
+            };
+        }
+
+        private void SetCameraWorkModeToShootPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            SetCameraWorkMode(CameraWorkMode.SHOOT_PHOTO);
+        }
+
+        private void SetCameraModeToRecord_Click(object sender, RoutedEventArgs e)
+        {
+            SetCameraWorkMode(CameraWorkMode.RECORD_VIDEO);
+        }
+
+        private async void SetCameraWorkMode(CameraWorkMode mode)
+        {
+            if (DJISDKManager.Instance.ComponentManager != null)
+            {
+                CameraWorkModeMsg workMode = new CameraWorkModeMsg
+                {
+                    value = mode,
+                };
+                var retCode = await DJISDKManager.Instance.ComponentManager.GetCameraHandler(0, 0).SetCameraWorkModeAsync(workMode);
+                if (retCode != SDKError.NO_ERROR)
+                {
+                    lblMode.Text = "Mode : " + mode.ToString() + "échoué." + "("+retCode.ToString()+")";
+                }
+            }
+            else
+            {
+                //non enregistré
+                Console.WriteLine("Licence non enregistrée.");
+            }
+        }
+    }
+}
